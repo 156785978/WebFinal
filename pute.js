7e918be651b1dec41fd36cf3a59c1fba65a22c42
ChunWei <dsjfbnkjsbdfkjs@gmail.com>
2021-01-07 20:44:30 +0800

    handle_data_exception

--- pute.js
+++ pute.js
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const bedQuery = require('./lib/bedQuery');
+const bedQuery_excep = require('./lib/bedQuery_excep');
 let p = fs.readFileSync('./data/bed.json');
+let p_e = fs.readFileSync('./data/bed_extrahandle.json');
 p = JSON.parse(p);
-
+p_e = JSON.parse(p_e);
 (async () => {
   const browser = await puppeteer.launch({
     headless: false
@@ -39,7 +41,19 @@ p = JSON.parse(p);
     }
     info.push(data);
   }
-
+  for (let i = 0; i < p_e.length; i++) {
+    console.log("going to " + p[i]['url']);
+    await page.goto(p_e[i]['url'], {
+      waitUntil: "networkidle2",
+      timeout: 0
+    });
+    data = {
+      hosp: p_e[i]['hosp'],
+      city: p_e[i]['city'],
+      info: await page.evaluate(bedQuery_excep)
+    }
+    info.push(data);
+  }
   if (!fs.existsSync("./data")) fs.mkdirSync("./data");
   //write data to json
 
