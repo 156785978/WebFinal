3d92ea3ab1c71c67c611e49f026d2f3932f8f574
ChunWei <dsjfbnkjsbdfkjs@gmail.com>
2021-01-09 08:18:06 +0800

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http=require('http');
+/*var http=require('http');
 var fs = require('fs');
 
 var server =http.createServer(function(req,res){
@@ -11,6 +11,15 @@ var server =http.createServer(function(req,res){
     }
     res.end(JSON.stringify(myObj));
     });
-
+const port = process.env
 server.listen(3000,'127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
+*/
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('help');
+})
