e3c7f62690643b233e06c5c1c7cc8dc90909e555
ChunWei <dsjfbnkjsbdfkjs@gmail.com>
2021-01-09 22:59:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-/*var http=require('http');
-var fs = require('fs');
-
-var server =http.createServer(function(req,res){
-    console.log('request was made: '+req.url);
-    res.writeHead(200,{'Content-Type':'application/json'});
-    var myObj={
-        name:'rr',
-        job:'no',
-        age:29,
-    }
-    res.end(JSON.stringify(myObj));
-    });
-const port = process.env
-server.listen(3000,'127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
-*/
-
 const express = require('express');
 const app = express();
 const importdata = require('./bedInfo.json');
